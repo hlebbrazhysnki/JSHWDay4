1ae8d154f986720c6ffabb29d3f89a3f901aba17
Hleb Brazhynski <hleb200217@gmail.com>
2021-10-15 02:30:37 -0400

    JSHomeworkDay3

---
+++ index.js
@@ -0,0 +1,262 @@
+// 1
+
+// in-line Comment
+
+/* this is
+ a multi line comment */
+
+//  2
+// data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+
+var myName = "Beau"
+
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14
+
+
+// 3
+
+var a;
+
+var b = 2;
+
+console.log(a)
+
+a = 7;
+
+b = a;
+
+console.log(a)
+
+// 4
+
+var a = 9;
+
+// 5
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 6
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9001;
+
+// 7
+
+var sum = 10 + 10;
+console.log(sum)
+
+// 8
+
+var difference = 45 - 33;
+
+// 9
+
+var product = 8 * 10;
+
+// 10
+
+var quotient = 66 / 33;
+
+
+// 11
+
+var myVar = 87;
+
+myVar++;
+
+// 12
+
+var myVar = 11;
+
+myVar = myVar - 1;
+
+myVar--;
+
+// 13
+
+var ourDecimal = 5.7;
+
+var myDecimal = 3.14159
+
+// 14
+
+var product = 2.0 * 2.5;
+console.log(product)
+
+// 15
+
+var quotient = 4.4 / 2.0;
+
+// 16
+
+var remainder;
+
+reminder = 11% 3;
+
+5 % 2 = 1 
+Math.floor(5 / 2) = 2 (Quotient)
+
+// 17
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+a += 12;
+b += 9 ;
+c += 7;
+
+// 18
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 19
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5;
+b *= 3 ;
+c *= 10;
+
+// 20
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+
+a /= 12;
+b /= 4;
+c /= 11
+
+// 21
+
+var myName = "your name";
+
+var myFirstName = "Corbett";
+var myLastName = "Croslin";
+
+// 22
+
+'Peter said, \'Have a nice day\' '
+
+var myStr = "I am a 'double quoted' string inside 'double quotes'.";
+
+// 23
+
+var ourStr = "I come first. " + "I come second.";
+
+var myStr = "This is the start. " + "This is the end.";
+
+// 24
+
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+// 25
+
+var ourName = "freeCodCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+var myName ="Corbett"
+var myStr = "My name is " + myName + " and I am well!"
+
+// 26
+
+var anAdjective = "awesome!";
+var ourStr = "freCodeCamp is ";
+ourStr += anAdjective;
+
+var someAdjective = "worthwhile"
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
+
+// 27
+
+
+
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+// 28
+
+var firstLetterOfFirstName
+var firstName = "Ada";
+var firstLetter = firstName[0];
+
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
+
+// 29
+
+var myStr = "Jello World";
+myStr[0] = "Hello World";
+
+// 30
+
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+var lastName = "Lovelace";
+
+var thirdLetterOfLastName = lastName[2];
+
+// 31
+
+var firstName = "Charles";
+var lastLetter = firstName[firstName.length - 1];
+
+var lastName = "Lovelace";
+
+var lastLetterOfLastName = lastName[lastName.length - 6];
+
+// 32
+
+var lastName = "Lovelace";
+
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+// 33
+
+var sentence = "It was really " + "hot" + ", and we " + "laughed" + " ourselves " + "silly" + ".";
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = "The " + myAdjective + ' ' + myNoun + ' ' + myVerb + ' ' + myAdverb; // Change 
